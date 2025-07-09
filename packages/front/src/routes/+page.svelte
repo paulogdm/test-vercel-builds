d424534b19699f2c7a61fde0672fc5c87a765e3c
paulogdm <me@paulogdm.com>
2025-07-09 16:05:47 -0300

    testing 123

--- packages/front/src/routes/+page.svelte
+++ packages/front/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	const message = formatMessage('Frontend User');
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit - Test 123</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <div>
